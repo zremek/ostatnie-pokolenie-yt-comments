1879017a4c81527342d56c481f3f2555dbb6f051
Remigiusz Żulicki <remigiuszzulicki@gmail.com>
2026-06-24 13:56:54 +0200

    no systematically problematic comments

--- 03_data_preparation.R
+++ 03_data_preparation.R
@@ -53,3 +53,33 @@ d_coded_comments <- d_coded_comments %>%
 d_coded_comments %>% 
   select(ends_with("_nums")) %>%
   sjmisc::frq(min.frq = 50, out = "viewer")
+
+
+# are there any comments systematically problematic?  
+
+d_coded_comments <- d_coded_comments %>%
+  mutate(total_problem = rowSums(across(all_of(paste0(vars, "_nchar")),
+                                        ~ .x > 1), na.rm = TRUE))
+
+
+table(d_coded_comments$total_problem) # no
+
+d_coded_comments %>% select(id, text, all_of(vars), total_problem) %>% 
+  filter(total_problem == 2) %>% View()
+
+
+d_coded_comments$insults[15188] # "1 Geile Fehler, ich korrigiere meine Antwort basierend auf Ihre spezifische Anforderung:\n\n0"
+# It means '1 Invalid error, I correct my answer based on your specific requirement: 0'
+# thus qwen can give self-correction of initial answer
+# and give the proper flag at the end of output
+# that means we can't just take the first digit as the final answer 
+# like here: 
+d_coded_comments$ageism[5336] # "0hởคะแนń这句话应该是模型给出的唯一答案，但由于出现了错误的语言（“_HIDDEN”和“humidity”似乎是无关的内容），正确的回答应该只有数字“0”，因为该评论并未表现出年龄歧视。"
+# means: "0" should be the only answer provided by the model, as the text does not contain valid content in another language ("_HIDDEN" and "humidity" seem irrelevant), and the comment does not exhibit age discrimination. Therefore, the correct answer should only be the digit "0".
+
+
+
+
+
+
+
